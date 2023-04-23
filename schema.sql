8db2bb5e829365ba81ad52171979ab10a49c1b91
JenniferZhu98 <zyzhu9988@gmail.com>
2023-04-23 17:35:55 -0400

    update notebook and schema
    update notebook and schema for sunrise

--- schema.sql
+++ schema.sql
@@ -1,4 +1,39 @@
 
+CREATE TABLE IF NOT EXISTS hourlyweather(
+    hourlyweatherId INTEGER PRIMARY KEY AUTOINCREMENT,
+    datetime DATETIME, 
+    HourlyWindSpeed FLOAT,
+    HourlyPrecipitation FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS dailyweather(
+    dailyweatherId INTEGER PRIMARY KEY AUTOINCREMENT,
+    datetime DATETIME, 
+    DailyAverageWindSpeed FLOAT,
+    DailyPrecipitation FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS taxitrips(
+    taxitripsId INTEGER PRIMARY KEY AUTOINCREMENT,
+    start_datetime DATETIME, 
+    start_lat FLOAT,
+    start_lon FLOAT,
+    end_lat FLOAT,
+    end_lon FLOAT,
+    distance FLOAT,
+    tip_amount FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS ubertrips(
+    ubertripsId INTEGER PRIMARY KEY AUTOINCREMENT,
+    start_datetime DATETIME, 
+    start_lat FLOAT,
+    start_lon FLOAT,
+    end_lat FLOAT,
+    end_lon FLOAT,
+    distance FLOAT
+);
+
 CREATE TABLE IF NOT EXISTS sunrise(
     sunriseId INTEGER PRIMARY KEY AUTOINCREMENT,
     datetime DATETIME, 
